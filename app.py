a10d67d7832aa9c18bfdd5eecb1830d5feb6a082
Jack <jack@jack.engineering>
2020-07-02 17:46:40 -0400

    working video
    old
    
    
    test
    
    
    was working
    
    
    some formatting
    
    
    threaded = true

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
+from flask import Flask, render_template, Response, request, redirect
 import os
-
-from flask import Flask, render_template, request, redirect
-
-from camera_control import camera as cam
-
-# Arducam Documentation: https://github.com/ArduCAM/RaspberryPi
-
-app: Flask = Flask(__name__)
+from camera_control.camera_pi import Camera
+import camera_control.camera_pi as cams
+app = Flask(__name__)
 
 temp_val = 512
 date = None
@@ -15,73 +11,66 @@ bio = None
 
 @app.route('/')
 def index():
-	global temp_val
-	return render_template('index.html', temp=str(temp_val))
+    global temp_val
+    return render_template('index.html', temp=str(temp_val))
 
 
 @app.route('/', methods=['POST'])
 def index_post():
-	global bio, date
-	posts = request.form.getlist('posts')
-	bio = request.form['bio']
-	date = request.form['dater']
-	return render_template("index.html", posts=posts)
+    global slots, bio, date
+    posts = request.form.getlist('posts')
+    bio = request.form['bio']
+    date = request.form['dater']
+    slots = posts
+    return render_template("index.html", posts=posts)
 
 
 @app.route('/feed')
 def feed():
-	return Response(cam.gen(cam.Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/stageup')
-def stage_up():
-	return 'MOVE STAGE UP'
-
-
-@app.route('/stagedown')
-def stage_down():
-	return 'MOVE STAGE DOWN'
+    return Response(cams.gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/focusup')
 def focus_up():
-	global temp_val
-	if temp_val < 1000:
-		temp_val += 10
-
-	value = (temp_val << 4) & 0x3ff0
-	dat1 = (value >> 8) & 0x3f
-	dat2 = value & 0xf0
-	os.system("i2cset -y 0 0x0c %d %d" % (dat1, dat2))
-	return redirect('/')
+    global temp_val
+    if temp_val < 1000:
+        temp_val += 10
+    else:
+        temp_val = temp_val
+    value = (temp_val << 4) & 0x3ff0
+    dat1 = (value >> 8) & 0x3f
+    dat2 = value & 0xf0
+    os.system("i2cset -y 0 0x0c %d %d" % (dat1, dat2))
+    return redirect('/')
 
 
 @app.route('/focusdown')
 def focus_down():
-	global temp_val
-	if (temp_val >= 12):
-		temp_val -= 10
+    global temp_val
+    if (temp_val >= 12):
+        temp_val -= 10
 
-	value = (temp_val << 4) & 0x3ff0
-	dat1 = (value >> 8) & 0x3f
-	dat2 = value & 0xf0
-	os.system("i2cset -y 0 0x0c %d %d" % (dat1, dat2))
-	return redirect('/')
-
-
-@app.route('/upload')
-def upload():
-	os.system(
-		'scp ../videotrial.h264 root@134.122.113.166:../home/jupyter-jack/scratch/Videos/'
-	)
-	return render_template("index.html")
+    value = (temp_val << 4) & 0x3ff0
+    dat1 = (value >> 8) & 0x3f
+    dat2 = value & 0xf0
+    os.system("i2cset -y 0 0x0c %d %d" % (dat1, dat2))
+    return redirect('/')
 
 
 @app.route('/record')
 def record():
-	cam.Camera.rec(10, date, bio)
-	return render_template("index.html")
+    Camera.rec(10, date, bio)
+    return render_template("index.html")
+
+
+@app.route('/upload')
+def upload():
+    os.system(
+        "scp ../videotrial.h264 root@134.122.113.166:../home/jupyter-jack/scratch/Videos"
+    )
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', threaded=True)
