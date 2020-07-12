04518602cc437b0447732f84060578791b6850ca
Brendan Murphy <thesimplekid@gmail.com>
2020-07-11 22:02:57 -0400

    gets info from form

--- flask/app.py
+++ flask/app.py
@@ -1,27 +1,90 @@
-from flask import Flask, render_template, Response, request, redirect
+import logging
 import os
-from camera_control.camera_pi import Camera
-import camera_control.camera_pi as cams
-app = Flask(__name__)
+
+#import camera_control.camera_pi as cams
+import forms
+import models
+#from camera_control.camera_pi import Camera
+from flask import Flask, Response, redirect, render_template, request
+
+logging.basicConfig(filename='app.log',
+                    format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+logging.warning("New Run Starts Here")
+
+
+def create_app():
+    # TODO: move to wsgi??
+    app = Flask(__name__)
+    #  REVIEW: : this needs to be changed
+    app.secret_key = 'development key'
+
+    return app
+
+
+app = create_app()
+
 
 temp_val = 512
 date = None
 bio = None
 
 
+def get_post_info(wtforms_list):
+    # from flask multi tissue tracking
+    # converts the list of form data in list of number and type or empty if that post is not in use
+    count = 0
+    li = []
+    for entry in wtforms_list:
+        if entry.data['post_in_use'] is True:
+            count = count + 1
+            li.append(
+                entry.data['tissue_num'] + "," + entry.data['type_of_tissue'])
+        else:
+            li.append("empty")
+    return count, li
+
+
+'''
 @app.route('/')
 def index():
     global temp_val
-    return render_template('index.html', temp=str(temp_val))
+    form = forms.upload_to_b_form()
+
+    return render_template('index.html', temp=str(temp_val), form=form)
+'''
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index_post():
-    global bio, date
-    posts = request.form.getlist('posts')
-    bio = request.form['bio']
-    date = request.form['dater']
-    return render_template("index.html", posts=posts)
+    form = forms.upload_to_b_form()
+    if request.method == 'POST':
+        # TODO: add form validation
+        # REVIEW: delete gloabl???
+        global bio, date
+        # is a list of info about tissue 'empty' if string not in use
+        # 'tissue_number,tissue_type' if it is in use
+        li_of_post_info = get_post_info(form.post.entries)
+        logging.info(li_of_post_info)
+
+        # REVIEW: ideally would like to make these drop downs for experminet and bio reactor
+
+        # checks if experiment exsits if it does makes it
+        experiment_num = form.experiment_num.data
+        logging.info(experiment_num)
+        if models.get_experiment(experiment_num) is None:
+            models.insert_experiment(experiment_num)
+
+            # checks if experiment exsits if it does makes it
+        bio_reactor_num = form.bio_reactor_num.data
+        logging.info(bio_reactor_num)
+        if models.get_bio_reactor(bio_reactor_num) is None:
+            models.insert_bio_reactor(bio_reactor_num)
+
+        # add the tissues to the databse as children of the vid, experiment and bio reactor
+        add_tissues(li_of_post_info, experiment_num,
+                    bio_reactor_num, new_video_id)
+    else:
+        return render_template("index.html", form=form)
 
 
 @app.route('/feed')
@@ -38,6 +101,7 @@ def stage_up():
 @app.route('/stagedown')
 def stage_down():
     return redirect('/')
+
 
 @app.route('/focusup')
 def focus_up():
---
+++ flask/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import (BooleanField, DecimalField, FieldList, FileField,
+                     FormField, HiddenField, IntegerField, SelectField,
+                     SelectMultipleField, StringField, SubmitField, validators,
+                     widgets)
+from wtforms.fields.html5 import DateField
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+class Tissue_Samples(FlaskForm):
+    post_in_use = BooleanField(
+        'Check If Post In Use')
+    tissue_num = StringField('Tissue Number')
+    type_of_tissue = StringField('Type of Tissue')
+
+
+class upload_to_b_form(FlaskForm):
+    date_recorded = DateField('Date Recorded', [validators.Required()])
+    post = FieldList(FormField(Tissue_Samples), min_entries=6)
+    frequency = DecimalField('Enter the Frequency')
+    bio_reactor_num = IntegerField('Enter Bio Reactor Number')
+    experiment_num = IntegerField('Enter Experiment number')
+    submit = SubmitField('Submit')
+
+
+class PickVid(FlaskForm):
+    form_name = HiddenField('Form Name')
+    experiment = SelectField('Experiment', id='select_experiment')
+    date = SelectField('Date', id='select_date')
+    vids = SelectField('Vids', id='select_vids')
+    submit = SubmitField('Select Video')
